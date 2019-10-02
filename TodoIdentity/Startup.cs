474a98a3f038df336e88c314b65901e9bbaf48ef
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2019-10-02 17:16:17 +0200

    alapállapot

--- TodoIdentity/Startup.cs
+++ TodoIdentity/Startup.cs
@@ -20,6 +20,8 @@ namespace TodoIdentity
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
+             
         }
 
         public IConfiguration Configuration { get; }

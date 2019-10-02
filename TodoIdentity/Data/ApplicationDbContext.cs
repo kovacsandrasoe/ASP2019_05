3e00a7013490d1e696cb4a4e2f6da6c297127a29
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2019-10-02 17:55:57 +0200

    Data tables added

--- TodoIdentity/Data/ApplicationDbContext.cs
+++ TodoIdentity/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TodoIdentity.Models;
 
 namespace TodoIdentity.Data
 {
@@ -12,5 +13,8 @@ namespace TodoIdentity.Data
             : base(options)
         {
         }
+
+        public DbSet<Job> Jobs { get; set; }
+        public DbSet<Todo> Todos { get; set; }
     }
 }
